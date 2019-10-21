8a4ca0606fc021c64f8676ba06d1ce19c2116047
Mavetane <collenmaphike25@gmail.com>
2019-10-21 10:35:06 +0200

    Project complete

---
+++ Projects/front-end-projects/drum-machine/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './components/Buttons'
+import Buttons from './components/Buttons';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Drum Machine!</header>
+      <Buttons/>
+    </div>
+  );
+}
+
+export default App;
