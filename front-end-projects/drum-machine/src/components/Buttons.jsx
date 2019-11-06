41c681a8ffa5c91f66c43fac53e896920880d15b
Mavetane <collenmaphike25@gmail.com>
2019-11-06 10:42:52 +0200

    project complete

---
+++ front-end-projects/drum-machine/src/components/Buttons.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { audios, pianoAudios } from '../config/index';
+
+class Buttons extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            play: false,
+            switch: false,
+            power: false,
+            beat: ""
+        }
+    }
+    controlSwitch = () => {
+        this.setState({
+            switch: !this.state.switch
+        })
+    }
+    togglePower = () => {
+        this.setState({ power: !this.state.power })
+    }
+
+    handleClick = (sound) => {
+        if (this.state.power === true) {
+            if (this.state.play !== true) {
+                let audio = new Audio(sound.url)
+                return audio.play()
+            }
+            this.setState({
+                play: !this.state.play
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="Wrapper">
+                {!this.state.switch == true ?
+                    <div className="Buttons">
+                        {audios.map(sound => {
+                            return <div className="Button-mods"><button onClick={() => this.handleClick(sound)} id={sound.name}>{sound.letter}</button></div>
+                        })}
+                    </div> : <div className="Piano">{pianoAudios.map(sound => {
+                        return <div><button onClick={() => this.handleClick(sound)} id={sound.name}>{sound.letter}</button></div>
+                    })}</div>}
+
+                <div className="Controls">
+                    <p>Power</p>
+                    <label className="Power">
+                        <input type="checkbox" onClick={() => this.togglePower()} />
+                        <span className="Slider-two"></span>
+                    </label><br />
+                    <div className="Blank">{this.state.beat}</div>
+                    <br />
+                    <div ClassName="Box">
+                        <center><div id="Value"></div></center>
+                    </div>
+                    <div className="Volume-container">
+                        <input type="range" min="0" max="100" value="50" className="Volume-slider" id="Volume-slider" />
+                    </div><br />
+                    {!this.state.switch == true ? <h4>Heater</h4> : <h4>Piano</h4>}
+                    <label className="Switch">
+                        <input type="checkbox" onClick={() => this.controlSwitch()} />
+                        <span className="Slider"></span>
+                    </label>
+                </div>
+            </div>
+        );
+    }
+}
+
+Buttons.propTypes = {
+};
+
+export default Buttons;
---
+++ front-end-projects/drum-machine/src/config/index.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+
+export const audios = [{
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    name: 'Heater-1',
+    letter: 'Q'
+},
+{
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    name: 'Heater-2',
+    letter: 'W'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    name: 'Heater-3',
+    letter: 'E'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    name: 'Heater-4',
+    letter: 'A'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    name: 'Heater-6',
+    letter: 'S'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    name: 'Dsc_Oh',
+    letter: 'D'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    name: 'Kick_n_Hat',
+    letter: 'Z'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    name: 'RP4_KICK1',
+    letter: 'X'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    name: 'Cev_H2',
+    letter: 'C'
+}
+]
+
+export const pianoAudios = [{
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+    name: 'Chord_1',
+    letter: 'Q',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+    name: 'Chord_2',
+    letter: 'W',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+    name: 'Chord_3',
+    letter: 'E',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+    name: 'Give_us_a_light',
+    letter: 'A',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+    name: 'Dry_ohh',
+    letter: 'S',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+    name: 'Bld_H1',
+    letter: 'D',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+    name: 'puncy_kick_1',
+    letter: 'Z',
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+    name: 'side_stick_1',
+    letter: 'X'
+}, {
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+    name: 'Brk_Snr',
+    letter: 'C',
+}
+]
