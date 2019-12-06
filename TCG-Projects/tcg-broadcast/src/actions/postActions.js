ebca93fbe73ce33d2874e755566f0c12e6bc3348
Mavetane <collenmaphike25@gmail.com>
2019-12-06 12:23:36 +0200

    web sockets complete

--- TCG-Projects/tcg-broadcast/src/actions/postActions.js
+++ TCG-Projects/tcg-broadcast/src/actions/postActions.js
@@ -1,26 +1,29 @@
 import { FETCH_POSTS, NEW_POST } from './types';
 
+const url = "ws://ac88a44a6935711e982b602f197ebe6f-1529281652.eu-west-2.elb.amazonaws.com/chat/"
+        var exampleSocket = new WebSocket(url);
+
 export const fetchPosts = () => dispatch => {
-    fetch('http://ac88a44a6935711e982b602f197ebe6f-1529281652.eu-west-2.elb.amazonaws.com/chat/person/')
-        .then(res => res.json())
-        .then(data => dispatch({
+
+    exampleSocket.onopen = function (event) {
+        console.log("connected");
+    };
+    exampleSocket.onmessage = function (event) {
+        const messages = JSON.parse(event.data);
+        console.log("message", messages)
+        dispatch({
             type: FETCH_POSTS,
-            payload: data
-        }));
+            payload: messages
+        })
+    }
 }
 
 export const createPost = (postData) => dispatch => {
-    
-    fetch('http://ac88a44a6935711e982b602f197ebe6f-1529281652.eu-west-2.elb.amazonaws.com/chat/person/', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(postData)
-        })
-            .then(res => console.log(res))
-            .then(data => dispatch({
-                type: NEW_POST,
-                payload: data
-            }))
+
+    exampleSocket.send(JSON.stringify(postData))
+    console.log("This is post data", postData)
+    dispatch({
+        type: NEW_POST,
+        payload: postData
+    })
 }
